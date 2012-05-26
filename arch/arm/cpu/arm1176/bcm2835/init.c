f8dd8404e77d250226a83a5ed85df06ec2ec0d4f
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2012-05-25 23:44:07 -0700

    Clean up and use memory information from config

--- arch/arm/cpu/arm1176/bcm2835/init.c
+++ arch/arm/cpu/arm1176/bcm2835/init.c
@@ -19,8 +19,8 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <config.h>
 #include <common.h>
-#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -29,9 +29,9 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int arch_cpu_init(void)
 {
-        volatile int *aux_enables = (int*)(BCM2835_UART_BASE + 0x4);
-        volatile int *gpio_fsel1 = (int*)(BCM2835_GPIO_BASE + 0x04);
-        int v;
+	volatile int *aux_enables = (int*)(BCM2835_UART_BASE + 0x4);
+	volatile int *gpio_fsel1 = (int*)(BCM2835_GPIO_BASE + 0x04);
+	int v;
 
 	// Configure GPIO pins 14 and 15 to serve as ALT5 func (TXD1 RXD1)
 	v = *gpio_fsel1;
@@ -41,15 +41,12 @@ int arch_cpu_init(void)
 	v |= ((2 << (5*3)));
 	*gpio_fsel1 = v;
 
-        // enable UART
-        v = *aux_enables;
-        v |= 1;
-        *aux_enables = v;
-
-	icache_enable();
+	// enable UART
+	v = *aux_enables;
+	v |= 1;
+	*aux_enables = v;
 
 	/* Show only 128Mb of RAM, to not touch GPU data */
-	gd->ram_size = 0x04000000;
-
+	gd->ram_size = PHYS_SDRAM_1_SIZE;
 	return 0;
 }
